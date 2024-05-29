589816ceb8f831b3d0f35fe5494a70269fd3926d
Nayelipadron24 <nayelii.jaqui.p.v@gmail.com>
2024-05-29 12:10:18 -0600

    Barra de navegacion

--- njpv-shop/src/app/app.module.ts
+++ njpv-shop/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimengModule } from './primeng.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegisterComponent } from './componentes/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     PrimengModule
   ],
